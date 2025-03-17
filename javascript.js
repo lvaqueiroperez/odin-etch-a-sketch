4b1ed721d450a6d17ac730ec142f5067a676b43a
Luis <43774737+lvaqueiroperez@users.noreply.github.com>
2025-03-17 13:34:01 +0100

    Change creation of divs logic in js Now each created div in js has two more divs inside of it, representing the squares that will be one vs the other.

--- javascript.js
+++ javascript.js
@@ -1,7 +1,16 @@
-const squareContainer = document.querySelector("#squareContainer");
+const squaresMainContainer = document.querySelector("#squaresMainContainer");
 
-for (let i = 0; i < 256; i++) {
-    const squareDiv = document.createElement("div");
-    squareDiv.setAttribute("class", "square");
-    squareContainer.appendChild(squareDiv);
+for (let i = 0; i < 128; i++) {
+
+    const squareContainer = document.createElement("div");
+    const squareDiv1 = document.createElement("div");
+    const squareDiv2 = document.createElement("div");
+    squareContainer.setAttribute("class", "squareContainer");
+    squareDiv1.setAttribute("class", "square");
+    squareDiv2.setAttribute("class", "square");
+
+    squareContainer.appendChild(squareDiv1);
+    squareContainer.appendChild(squareDiv2);
+
+    squaresMainContainer.appendChild(squareContainer);
 }
