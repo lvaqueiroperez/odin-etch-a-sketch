058c4f3f28f06c6763663f7c8280ab6f6de80cc4
Luis <43774737+lvaqueiroperez@users.noreply.github.com>
2025-03-17 13:04:24 +0100

    Add basic css to square class and fix typo in js

--- javascript.js
+++ javascript.js
@@ -2,6 +2,6 @@ const squareContainer = document.querySelector("#squareContainer");
 
 for (let i = 0; i < 256; i++) {
     const squareDiv = document.createElement("div");
-    squareDiv.setAttribute("class", ".square");
+    squareDiv.setAttribute("class", "square");
     squareContainer.appendChild(squareDiv);
 }
