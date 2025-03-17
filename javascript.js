fb22289042213921becb590cb7203105a3da2062
Luis <43774737+lvaqueiroperez@users.noreply.github.com>
2025-03-17 18:11:25 +0100

    Move grid creation logic to printGrid function

--- javascript.js
+++ javascript.js
@@ -2,12 +2,14 @@ const squaresMainContainer = document.querySelector(".squaresMainContainer");
 
 const resizeButton = document.querySelector("#resizeButton");
 
+let gridDimensions = 16;
+
 resizeButton.addEventListener("click", () => {
-    const gridDimensions = +prompt("Please, enter the number of squares per row that you wish to see (max 100):", "");
+    gridDimensions = +prompt("Please, enter the number of squares per row that you wish to see (max 100):", "");
 
-    if(gridDimensions && gridDimensions >=0 && gridDimensions <=100) {
+    if (gridDimensions && gridDimensions >= 0 && gridDimensions <= 100) {
 
-      
+
 
     } else {
         alert("ERROR. Please enter a correct value!");
@@ -15,18 +17,19 @@ resizeButton.addEventListener("click", () => {
 
 });
 
-// 16x16 grid, 256 squares in total
-for (let i = 0; i < 16; i++) {
+function printGrid(gridDimensions) {
+    for (let i = 0; i < gridDimensions; i++) {
 
-    const squareContainer = document.createElement("div");
-    squareContainer.setAttribute("class", "squareContainer");
+        const squareContainer = document.createElement("div");
+        squareContainer.setAttribute("class", "squareContainer");
 
-    for (let j = 0; j < 16; j++) {
-        const squareDiv = document.createElement("div");
-        squareDiv.setAttribute("class", "square");
-        squareContainer.appendChild(squareDiv);
+        for (let j = 0; j < gridDimensions; j++) {
+            const squareDiv = document.createElement("div");
+            squareDiv.setAttribute("class", "square");
+            squareContainer.appendChild(squareDiv);
+        }
+
+        squaresMainContainer.appendChild(squareContainer);
+
     }
-
-    squaresMainContainer.appendChild(squareContainer);
-
-}
+};
