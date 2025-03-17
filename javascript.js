16650c9a05557200b2eadacbbc68e427f72b97d0
Luis <43774737+lvaqueiroperez@users.noreply.github.com>
2025-03-17 13:01:56 +0100

    Add square class to the created div squares

--- javascript.js
+++ javascript.js
@@ -1,6 +1,7 @@
 const squareContainer = document.querySelector("#squareContainer");
 
-for(let i = 0; i < 256; i++) {
+for (let i = 0; i < 256; i++) {
     const squareDiv = document.createElement("div");
+    squareDiv.setAttribute("class", ".square");
     squareContainer.appendChild(squareDiv);
 }
