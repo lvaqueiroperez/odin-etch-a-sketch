66dc27c0888ad2626ba858c2d5f98e29f50d67e5
Luis <43774737+lvaqueiroperez@users.noreply.github.com>
2025-03-17 14:24:04 +0100

    Add event listener to resizeButton WIP

--- javascript.js
+++ javascript.js
@@ -1,5 +1,11 @@
 const squaresMainContainer = document.querySelector(".squaresMainContainer");
 
+const resizeButton = document.querySelector("#resizeButton");
+
+resizeButton.addEventListener("click", () => {
+    const squaresPerRow = +prompt("Please, enter the number of squares per row that you wish to see (max 100):", "");
+});
+
 for (let i = 0; i < 128; i++) {
     const squareContainer = document.createElement("div");
     const squareDiv1 = document.createElement("div");
