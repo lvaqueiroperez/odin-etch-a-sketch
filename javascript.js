9b5a8ff3d500d711fa16935ba895355642b0d4a7
Luis <43774737+lvaqueiroperez@users.noreply.github.com>
2025-03-17 18:01:09 +0100

    Modify logic to create a 16x16 grid of squares

--- javascript.js
+++ javascript.js
@@ -6,16 +6,18 @@ resizeButton.addEventListener("click", () => {
     const squaresPerRow = +prompt("Please, enter the number of squares per row that you wish to see (max 100):", "");
 });
 
-for (let i = 0; i < 128; i++) {
-    const squareContainer = document.createElement("div");
-    const squareDiv1 = document.createElement("div");
-    const squareDiv2 = document.createElement("div");
-    squareContainer.setAttribute("class", "squareContainer");
-    squareDiv1.setAttribute("class", "square");
-    squareDiv2.setAttribute("class", "square");
+// 16x16 grid, 256 squares in total
+for (let i = 0; i < 16; i++) {
 
-    squareContainer.appendChild(squareDiv1);
-    squareContainer.appendChild(squareDiv2);
+    const squareContainer = document.createElement("div");
+    squareContainer.setAttribute("class", "squareContainer");
+
+    for (let j = 0; j < 16; j++) {
+        const squareDiv = document.createElement("div");
+        squareDiv.setAttribute("class", "square");
+        squareContainer.appendChild(squareDiv);
+    }
 
     squaresMainContainer.appendChild(squareContainer);
+
 }
